71c7f561093c5d54b50e40659a8fd4e408524409
Mussawar Akram <mussawarakram@gmail.com>
2021-05-23 19:08:59 +0100

    mussDevelopment - purge and reset repository for new implementation

---
+++ src/test/java/com/awin/coffeebreak/AwinCoffeeBreakApplicationTest.java
@@ -0,0 +1,14 @@
+package com.awin.coffeebreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AwinCoffeeBreakApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
